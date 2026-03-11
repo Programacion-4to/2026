720ec740a21422a4c3bdf7b73ce3bcea97363dae
fcalderan19 <fcalderan@fi.uba.ar>
2026-03-11 10:53:09 -0300

    fix: ejercicios archivos

--- U1: Matrices, Archivos y Manejo de Errores/EjerciciosArchivos.py
+++ U1: Matrices, Archivos y Manejo de Errores/EjerciciosArchivos.py
@@ -1,10 +1,23 @@
-## Ejercicios — Archivos
+"""
+Ejercicios — Archivos
 
-# Ejercicio 1: Crear un archivo numeros.txt y guardar 10 números ingresados por el usuario.
+Ejercicio 1: Crear a traves de codigo un archivo numeros.txt y guardar 10 números ingresados por el usuario.
 
-# Ejercicio 2: Leer el archivo numeros.txt y mostrar todos los números.
+Ejercicio 2: Leer el archivo numeros.txt y mostrar todos los números.
 
-# Ejercicio 3: Leer el archivo numeros.txt y calcular la suma de todos los números.
+Ejercicio 3: Leer el archivo numeros.txt y calcular la suma de todos los números.
 
-# Ejercicio 4: Guardar en un archivo información con el formato nombre,edad (ej: Ana,20 / Juan,18 / Pedro,22).
-#  Luego leerlo y mostrar: "Ana tiene 20 años".
+Ejercicio 4: Guardar en un archivo información con el formato nombre,edad (ej: Ana,20 / Juan,18 / Pedro,22).
+Luego leerlo y mostrar: "Ana tiene 20 años".
+
+Ejercicio 5: Crear un archivo a traves de codigo donde guarden informacion de 5 productos en formato csv (nombre, precio, cantidad). 
+Luego leer el archivo y mostrar el total a pagar por cada producto (precio * cantidad).
+
+Ejercicio 6: Simular una base de datos de usuarios en un csv a traves de codigo con las siguientes columnas (id,nombre,email,contraseña). 
+Recordar que una base de datos tiene un campo ID (identificador) que evita que aunque hayan los mismos valores, sean considerados como registros unicos.
+Devolver la informacion de las columnas y mostrarla como: "Esta tabla tiene las columnas: id, nombre, email, contraseña". 
+Luego leer el archivo y mostrar la informacion de fila y mostrarla en formato: "ID: 1, Nombre: Ana, Email: ..."
+
+Ejercicio 7: Dado el archivo anterior, hacer una funcion que busque la informacion de un usuario por su ID y 
+devuelva su nombre y email.
+"""
