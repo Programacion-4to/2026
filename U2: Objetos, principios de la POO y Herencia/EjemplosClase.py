439caa3cf2727d673a9f3dcbb60af764b880ec53
fcalderan19 <fcalderan@fi.uba.ar>
2026-03-20 12:02:14 -0300

    feat(U2-Objetos): Ejemplo explicativo en clase

---
+++ U2: Objetos, principios de la POO y Herencia/EjemplosClase.py
@@ -0,0 +1,73 @@
+class Animal:
+    def __init__(self, edad, colores):
+        self.__edad = edad
+        self.colores = colores
+        self.tiene_cola = True
+
+    def comer(self):
+        ...
+    
+    def cazar(self):
+        ...
+
+    def mover_la_cola(self):
+        if self.tiene_cola:
+            print("Moviendo la cola")
+        else:
+            print("No tiene cola")
+
+    def cumplir_anios(self):
+        self.__edad +=1
+
+    def hacer_ruido(self):
+        pass
+
+class Perro(Animal):
+    def __init__(self, edad, colores, raza):
+        super().__init__(edad, colores)
+        self.raza = raza
+
+    def hacer_ruido(self):
+        print("Guau")
+
+
+class Dueno:
+    def __init__(self, nom, gen, age):
+        self.__nombre = nom
+        self.__genero = gen
+        self.__edad = age
+        self.mascotas = []
+
+    def pasear(self):
+        if self.mascotas != []:
+            print("Paseando...")
+        else:
+            print(f"Lo siento {self.__nombre}, no tienes mascotas todavia")
+
+    def adoptar(self, animal: Animal):
+        if isinstance(animal, Animal):
+            self.mascotas.append(animal)
+
+
+perrito = Perro(1,"Marron","Caniche")
+perrito.hacer_ruido()
+print(perrito.raza)
+
+tigre = Animal(4,["Naranja","Negro"])
+tigre.hacer_ruido()
+
+
+# facundo = Dueno("facundo","M",21)
+# facundo.pasear()
+# facundo.adoptar(perrito)
+# facundo.pasear()
+# print(facundo.mascotas)
+
+
+
+
+"""
+Dueno -> List[Animal]
+
+
+"""
